06b7a11af36f97fb5981ea2e4c1e593bba367f16
Pantelis <120404207+pantelovich@users.noreply.github.com>
2024-12-07 10:44:43 +0000

    Delete Python directory

--- Python/Fuctions/defReturn.py
+++
@@ -1,9 +0,0 @@
-def formatName(fName, lName):
-    if fName == "" or lName == "":
-        return "Invalid input"
-    formatedfName = fName.title()
-    formatedlName = lName.title()
-    return f"{formatedfName} {formatedlName}"
-
-
-print(formatName(input("Enter your first name: "), input("Enter your last name: ")))
--- Python/Loops/loopsFruit.py
+++
@@ -1,8 +0,0 @@
-fruits = [ "apple","peach","pear" ]
-for fruit in fruits:
-    print(fruit.capitalize())
-    print(fruit + " Pie")
-
-fruits = ",".join(fruits)
-
-print(fruits.capitalize())
